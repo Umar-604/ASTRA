d6aabc6a42b11336680b1e2a0efcad3e652ef1aa
Umar <umarr.k220@gmail.com>
2025-11-17 20:43:38 +0500

    Create dashboard.js

---
+++ fontend/dashboard.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".sidebar-nav a");
+    const bodyEl = document.body;
+    const themeToggle = document.getElementById("themeToggle");
+    const rootEl = document.documentElement;
+    const accentSwatches = document.querySelectorAll(".accent-swatch");
