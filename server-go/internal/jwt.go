a3873b30ef8422db9bc167e59370d4d0bf145fd7
Umar <umarr.k220@gmail.com>
2026-01-21 21:17:24 +0500

    Update jwt.go

--- server-go/internal/jwt.go
+++ server-go/internal/jwt.go
@@ -6,3 +6,9 @@ import (
 
     jwt "github.com/golang-jwt/jwt/v5"
 )
+
+func VerifyJWT(tokenString string) (map[string]any, error) {
+    secret := os.Getenv("JWT_SECRET")
+    if secret == "" {
+        return nil, errors.New("JWT_SECRET not set")
+    }
