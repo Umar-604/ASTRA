8413d1e9b2002a55787e7588ee0938457c597278
Umar <umarr.k220@gmail.com>
2026-01-30 17:03:13 +0500

    Update endpoint_predictor.py

--- ai_engine/src/endpoint_predictor.py
+++ ai_engine/src/endpoint_predictor.py
@@ -6,3 +6,9 @@ import joblib
 import numpy as np
 import os
 import warnings
+
+# Suppress TensorFlow mutex warnings
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
