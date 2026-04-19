63ef0925f96fb44c487c47d94628fade85f418b6
AyelaIsrarHaqqani <151562550+AyelaIsrarHaqqani@users.noreply.github.com>
2026-04-19 18:52:20 +0500

    Update train_endpoint.py

--- ai_engine/train_endpoint.py
+++ ai_engine/train_endpoint.py
@@ -6,3 +6,8 @@ Supports Isolation Forest, Autoencoder, and LSTM models
 import pandas as pd
 import numpy as np
 import joblib
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    classification_report,
