fab88ea12a615fdbed5c0f569551b90486dafeae
AyelaIsrarHaqqani <151562550+AyelaIsrarHaqqani@users.noreply.github.com>
2026-04-19 18:53:51 +0500

    Update train_endpoint.py

--- ai_engine/train_endpoint.py
+++ ai_engine/train_endpoint.py
@@ -25,3 +25,5 @@ os.environ['OMP_NUM_THREADS'] = '1'
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 os.environ['MKL_NUM_THREADS'] = '1'
 os.environ['NUMEXPR_NUM_THREADS'] = '1'
+# Suppress specific warnings
+warnings.filterwarnings('ignore', category=UserWarning, module='tensorflow')
