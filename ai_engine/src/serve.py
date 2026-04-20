1a2c41c45589f11630b1315d29a9a29da3f5e45b
Umar <umarr.k220@gmail.com>
2026-04-20 12:35:40 +0500

    Update serve.py

--- ai_engine/src/serve.py
+++ ai_engine/src/serve.py
@@ -132,3 +132,12 @@ def _wire_postgres_logging() -> None:
         # Emit one line to ensure table exists and first insert happens
         logging.getLogger(__name__).info("Startup: PostgreSQL logging wired")
     except Exception:
+        pass
+
+_wire_postgres_logging()
+
+# Suppress XGBoost mutex warnings
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
