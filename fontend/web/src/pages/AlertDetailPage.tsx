103fcacbed7d083b1173c0b042c798132016d279
AyelaIsrarHaqqani <151562550+AyelaIsrarHaqqani@users.noreply.github.com>
2026-04-20 19:02:29 +0500

    Update AlertDetailPage.tsx

--- fontend/web/src/pages/AlertDetailPage.tsx
+++ fontend/web/src/pages/AlertDetailPage.tsx
@@ -2,3 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { getEvent } from '@api/events';
 import { verifyIntegrityPost } from '@api/audit';
+import { acknowledgeAlert } from '@api/alerts';
+import { getResponseHistory } from '@api/response';
+import { BackButton } from '@components/ui/BackButton';
+import type { EventSummary, AlertItem, AlertStatus, ResponseHistoryItem } from '@app-types/api';
+import { getAlertScoreDisplay } from '../utils/alertScore';
